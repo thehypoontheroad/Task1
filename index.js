adc99295ee61e4750e56562e887e490bb6cb5eab
Divya Chaudhary <divyachaudhary@192.168.1.25>
2025-01-29 14:11:27 +0545

    new file of javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+// var firstNumber=5
+// var secondNumber=6
+
+// var ans=firstNumber+secondNumber
+
+// console.log("answer of addition",ans)
+
+
+// operators
+// arithematic operators
+// + - * / \ %
+// assignment operators
+// =
+
+// let firstNumber=10
+// let secondNumber=20
+
+// let ans=firstNumber+secondNumber
+
+// console.log("answer of addition,ans")
+
+// let first_Number=10
+// let second_Number=20
+
+// let Ans=firstNumber-secondNumber
+
+// console.log("answer of subtraction,ans")
+let number=5
+if (number>0) {
+    console.log("number is positive")
+} else{
+    console.log("number is negative")
+}
+
+
+
+
+
+
